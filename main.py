78b1cf79506c282ac5aec6b080ca2516d1d4b0b3
jvitorfranca <vitoorbr@gmail.com>
2018-11-09 09:53:11 -0300

    Adding SMOTE + ENN

--- main.py
+++ main.py
@@ -10,45 +10,49 @@ import pickle
 
 def main():
 
-    time = 60
+    time = 60*10
 
-    file = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+    runs = 6
 
-    file = ut.prune_data(file)
+    for j in range(0, runs):
 
-    X_train, X_test, Y_train, Y_test = sk.model_selection.train_test_split(file.drop('Churn', axis=1), file['Churn'], test_size=0.3)
+        file = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
-    automl = asc.AutoSklearnClassifier(
-                time_left_for_this_task=time+10,
-                per_run_time_limit=time,
-                initial_configurations_via_metalearning=0,
-                resampling_strategy='cv',
-                resampling_strategy_arguments={'folds': 5},
-                )
+        file = ut.prune_data(file)
 
-    # Choosing kappa as the metric
-    kappas = asc_t.metrics.make_scorer('kappa', sk.metrics.cohen_kappa_score)
+        X_train, X_test, Y_train, Y_test = sk.model_selection.train_test_split(file.drop('Churn', axis=1), file['Churn'], test_size=0.3)
 
-    automl.fit(X_train, Y_train, metric=kappas)
+        automl = asc.AutoSklearnClassifier(
+                    time_left_for_this_task=time+10,
+                    per_run_time_limit=time,
+                    initial_configurations_via_metalearning=0,
+                    # resampling_strategy='cv',
+                    # resampling_strategy_arguments={'folds': 5},
+                    )
 
-    automl.refit(X_train, Y_train)
+        # Choosing kappa as the metric
+        kappas = asc_t.metrics.make_scorer('kappa', sk.metrics.cohen_kappa_score)
 
-    # Getting the rank of models
-    i = 0
-    for key in automl.cv_results_.items():
-        if i < 5:
-            with open("log.txt", "a") as arch:
-                arch.write("{}\n\n".format(key))
-        else:
-            break
-        i = i + 1
+        automl.fit(X_train, Y_train, metric=kappas)
 
-    # Predicting the model
-    scores.predict_and_save(automl, X_test, Y_test, verbose=True, file="prediction.txt")
+        # automl.refit(X_train, Y_train)
 
-    # Save the model into binary code
-    filename = 'finalized_model.sav'
-    pickle.dump(automl, open(filename, 'wb'))
+        # Getting the rank of models
+        i = 0
+        for key in automl.cv_results_.items():
+            if i < 5:
+                with open("log.txt", "a") as arch:
+                    arch.write("{}\n\n".format(key))
+            else:
+                break
+            i = i + 1
+
+        # Predicting the model
+        scores.predict_and_save(automl, X_test, Y_test, verbose=True, file="prediction.txt")
+
+        # Save the model into binary code
+        filename = str(j)+'finalized_model.sav'
+        pickle.dump(automl, open(filename, 'wb'))
 
 if __name__ == "__main__":
     main()
---
+++ smoteenn.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import EditedNearestNeighbours
+
+
+def run(X=None, Y=None, random_state=42, smote_ratio="minority", smote_kind="regular", enn_ratio="all", enn_kind_sel="all", enn_n_neighbors=3, save_dist=False, file=None):
+
+    sm = SMOTE(random_state=random_state, ratio=smote_ratio, kind=smote_kind)
+
+    enn = EditedNearestNeighbours(random_state=random_state, ratio=enn_ratio, kind_sel=enn_kind_sel, n_neighbors=enn_n_neighbors)
+
+    X_resampled, Y_resampled = sm.fit_sample(X, Y)
+
+    if(save_dist):
+        with open(file, "a") as arch:
+           arch.write("SMOTE: " + str(Counter(Y_resampled)) + " ")
+
+    X_st, Y_st = enn.fit_sample(X_resampled, Y_resampled)
+
+    if(save_dist):
+        with open(file, "a") as arch:
+            arch.write("ENN:" + str(Counter(y_st))+"\n")
+
+    return X_st, Y_st
